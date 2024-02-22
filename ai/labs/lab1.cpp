8f2006943d3d5fead01f88f2918dcf602d690e88
nick-karamichael <karamichaeln@gmail.com>
2024-02-22 13:32:31 +0200

    number 3 finished
    finished submission 3

--- ai/labs/lab1.cpp
+++ ai/labs/lab1.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main()
 {
     string input;
-    string direction;
+    // string direction;
     cin >> input;
-    cin >> direction;
+    // cin >> direction;
     int num = 0;
     char gameboard[4][4];
+    int xAxis = -1;
+    int yAxis = -1;
 
-    // Initialize gameboard
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
@@ -18,6 +19,8 @@ int main()
             char curr = input[num++];
             if (curr == '#')
             {
+                xAxis = i;
+                yAxis = j;
                 gameboard[i][j] = '#';
             }
             else
@@ -26,47 +29,54 @@ int main()
             }
         }
     }
+    if (xAxis > 0)
+        cout << "UP" << endl;
+    if (xAxis < 3)
+        cout << "DOWN" << endl;
+    if (yAxis > 0)
+        cout << "LEFT" << endl;
+    if (yAxis < 3)
+        cout << "RIGHT" << endl;
 
-    // Move based on direction
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (gameboard[i][j] == '#' && direction == "RIGHT" && j < 3)
-            {
-                gameboard[i][j] = gameboard[i][j + 1];
-                gameboard[i][j + 1] = '#';
-                break;
-            }
-            else if (gameboard[i][j] == '#' && direction == "UP" && i < 3)
-            {
-                gameboard[i][j] = gameboard[i + 1][j];
-                gameboard[i + 1][j] = '#';
-                break;
-            }
-            else if (gameboard[i][j] == '#' && direction == "LEFT" && j > 0)
-            {
-                gameboard[i][j] = gameboard[i][j - 1];
-                gameboard[i][j - 1] = '#';
-                break;
-            }
-            else if (gameboard[i][j] == '#' && direction == "DOWN" && i > 0)
-            {
-                gameboard[i][j] = gameboard[i - 1][j];
-                gameboard[i - 1][j] = '#';
-                break;
-            }
-        }
-    }
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         if (gameboard[i][j] == '#' && direction == "RIGHT" && j < 3)
+    //         {
+    //             gameboard[i][j] = gameboard[i][j + 1];
+    //             gameboard[i][j + 1] = '#';
+    //             goto end;
+    //         }
+    //         else if (gameboard[i][j] == '#' && direction == "UP" && i > 0)
+    //         {
+    //             gameboard[i][j] = gameboard[i - 1][j];
+    //             gameboard[i - 1][j] = '#';
+    //             goto end;
+    //         }
+    //         else if (gameboard[i][j] == '#' && direction == "LEFT" && j > 0)
+    //         {
+    //             gameboard[i][j] = gameboard[i][j - 1];
+    //             gameboard[i][j - 1] = '#';
+    //             goto end;
+    //         }
+    //         else if (gameboard[i][j] == '#' && direction == "DOWN" && i < 3)
+    //         {
+    //             gameboard[i][j] = gameboard[i + 1][j];
+    //             gameboard[i + 1][j] = '#';
+    //             goto end;
+    //         }
+    //     }
+    // }
+    // end:
 
-    // Print gameboard
-    for (int i = 0; i < 4; ++i)
-    {
-        for (int j = 0; j < 4; ++j)
-        {
-            std::cout << gameboard[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    return 0;
+    //     for (int i = 0; i < 4; ++i)
+    //     {
+    //         for (int j = 0; j < 4; ++j)
+    //         {
+    //             std::cout << gameboard[i][j] << " ";
+    //         }
+    //         std::cout << std::endl;
+    //     }
+   return 0;
 }
