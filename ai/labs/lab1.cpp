7a2ca71428e4eba6409f4da3c663ed0a0d9e32e0
nick-karamichael <karamichaeln@gmail.com>
2024-02-22 10:38:46 +0200

    1st
    initial

---
+++ ai/labs/lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+    cin >> input;
+    int gameboard[4][4];
+    for(int i=)
+    cout << input << endl;
+    return 0;
+}
