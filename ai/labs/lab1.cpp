eb8e3b799bd28b0f7782e43ee64576fd14016ef2
nick-karamichael <karamichaeln@gmail.com>
2024-02-22 12:26:20 +0200

    finished lab 2
    lab 2 completion

--- ai/labs/lab1.cpp
+++ ai/labs/lab1.cpp
@@ -4,14 +4,18 @@ using namespace std;
 int main()
 {
     string input;
+    string direction;
     cin >> input;
+    cin >> direction;
     int num = 0;
     char gameboard[4][4];
+
+    // Initialize gameboard
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            char curr = input[num];
+            char curr = input[num++];
             if (curr == '#')
             {
                 gameboard[i][j] = '#';
@@ -20,22 +24,48 @@ int main()
             {
                 gameboard[i][j] = curr;
             }
-            num = num + 1;
         }
     }
+
+    // Move based on direction
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (gameboard[i][j] == '#' && direction == "RIGHT" && j < 3)
+            {
+                gameboard[i][j] = gameboard[i][j + 1];
+                gameboard[i][j + 1] = '#';
+                break;
+            }
+            else if (gameboard[i][j] == '#' && direction == "UP" && i < 3)
+            {
+                gameboard[i][j] = gameboard[i + 1][j];
+                gameboard[i + 1][j] = '#';
+                break;
+            }
+            else if (gameboard[i][j] == '#' && direction == "LEFT" && j > 0)
+            {
+                gameboard[i][j] = gameboard[i][j - 1];
+                gameboard[i][j - 1] = '#';
+                break;
+            }
+            else if (gameboard[i][j] == '#' && direction == "DOWN" && i > 0)
+            {
+                gameboard[i][j] = gameboard[i - 1][j];
+                gameboard[i - 1][j] = '#';
+                break;
+            }
+        }
+    }
+
+    // Print gameboard
     for (int i = 0; i < 4; ++i)
     {
-
         for (int j = 0; j < 4; ++j)
         {
-
-            if (j % 3 == 0)
-            {
-                std::cout << gameboard[i][j] << endl;
-            }
             std::cout << gameboard[i][j] << " ";
         }
-
         std::cout << std::endl;
     }
     return 0;
