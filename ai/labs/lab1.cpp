eae2c1d10fca20e13bafe984e8b0d5e8325159f8
nick-karamichael <karamichaeln@gmail.com>
2024-02-22 10:49:58 +0200

    1st part done
    finished first section

--- ai/labs/lab1.cpp
+++ ai/labs/lab1.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     string input;
     cin >> input;
-    int gameboard[4][4];
-    for(int i=)
-    cout << input << endl;
+    int num = 0;
+    char gameboard[4][4];
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            char curr = input[num];
+            if (curr == '#')
+            {
+                gameboard[i][j] = ' ';
+            }
+            else
+            {
+                gameboard[i][j] = curr;
+            }
+            num = num + 1;
+        }
+    }
+    for (int i = 0; i < 4; ++i)
+    {
+        
+        for (int j = 0; j < 4; ++j)
+        {
+            
+            std::cout << gameboard[i][j] << " ";
+        }
+        
+        std::cout << std::endl;
+    }
     return 0;
 }
