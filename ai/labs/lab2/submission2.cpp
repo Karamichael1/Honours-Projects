a3679f7defc41a57506d7193348757eab11fd96a
Karamichael1 <160006301+Karamichael1@users.noreply.github.com>
2024-02-27 15:31:33 +0200

    sub2

---
+++ ai/labs/lab2/submission2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+string boardOutput(string board){
+    for(int i=0;i<9;i++){
+        if(i%3==2){
+            cout<<endl;
+        }
+        if(board[i]=="."){
+            cout<<"_ ";
+        }else if(board[i]=="x"){
+            cout<<"x ";
+        }else if(board[i]=="o "){
+            cout<<"o ";
+        }
+}
+}
+int main(){
+    string board="";
+    cin>>board>>endl;
+    string final=boardOutput(board);
+}
