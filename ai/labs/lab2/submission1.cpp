ecb0331c4cd2327a6617f1f506fa846af22a85c0
nick-karamichael <karamichaeln@gmail.com>
2024-03-01 19:21:14 +0200

    sub1
    completed sub1

--- ai/labs/lab2/submission1.cpp
+++ ai/labs/lab2/submission1.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 #include <utility>
+#include <limits>
+
 using namespace std;
 
+typedef pair<int, int> Point;
 
+Point readPoint() {
+    int x, y;
+    char comma;
+    cin >> x >> comma >> y;
+    return make_pair(x, y);
+}
 
-string nashEquilibrium(int nash[],int size){
-string matrix[size][size];
-int count=0;
-for(int i=0;i<size;i++){
-    for(int j=0;j<size;j++){
-    matrix[i][j]=nash[count];
-    cout<<matrix[i][j];
-    count++;
+Point findNashEquilibrium(const vector<vector<Point>>& matrix) {
+    int matrixSize = matrix.size();
+
+    vector<int> maxValues(matrixSize, numeric_limits<int>::min());
+    for (int i = 0; i < matrixSize; ++i) {
+        for (int j = 0; j < matrixSize; ++j) {
+            maxValues[i] = max(maxValues[i], matrix[i][j].first);
+        }
     }
+
+    int maxRowIndex = 0;
+    for (int i = 1; i < matrixSize; ++i) {
+        if (maxValues[i] > maxValues[maxRowIndex]) {
+            maxRowIndex = i;
+        }
+    }
+
+    int maxColIndex = 0;
+    for (int j = 1; j < matrixSize; ++j) {
+        if (matrix[maxRowIndex][j].second > matrix[maxRowIndex][maxColIndex].second) {
+            maxColIndex = j;
+        }
+    }
+
+    return matrix[maxRowIndex][maxColIndex];
 }
 
-return "hello";
-}
+int main() {
+    int matrixSize;
+    cin >> matrixSize;
 
+    vector<vector<Point>> matrix(matrixSize, vector<Point>(matrixSize));
 
+    for (int i = 0; i < matrixSize; ++i) {
+        for (int j = 0; j < matrixSize; ++j) {
+            matrix[i][j] = readPoint();
+        }
+    }
 
-int main(){
-double size;
-cin>>size;
-int nash[int(pow(size,2.00))];
-for(int i=0;i<pow(size,2.00);i++){
-    cin>>nash[i];
-}
-cout<<nashEquilibrium(nash,size);
-return 0;
+    Point nashEquilibrium = findNashEquilibrium(matrix);
+
+    cout << nashEquilibrium.first << "," << nashEquilibrium.second << endl;
+
+    return 0;
 }
