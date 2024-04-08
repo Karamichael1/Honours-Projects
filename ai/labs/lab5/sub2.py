06adf67c75c1deee041d1e10a067d6f84229375e
nick-karamichael <karamichaeln@gmail.com>
2024-04-08 20:40:11 +0200

    sub2
    HMM

--- ai/labs/lab5/sub2.py
+++ ai/labs/lab5/sub2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def forward_algorithm(observations):
     n = len(observations)
-    F = np.zeros((n+1, 2))
+    F = np.zeros((n+3, 2))
     F[0, 0] = 0.5
     F[0, 1] = 0.5
     rainx2 = 0.7
@@ -15,15 +15,22 @@ def forward_algorithm(observations):
                   (F[t-1, 1] * (1-rainx2) + F[t-1, 0] * (1-rainXnone)) * (1-umbrellaXrain)
         F[t, 1] = (F[t-1, 0] * rainXnone + F[t-1, 1] * rainx2) * umbrellaXnone if observations[t-1] == 1 else \
                   (F[t-1, 1] * (1-rainXnone) + F[t-1, 0] * (1-rainx2)) * (1-umbrellaXnone)
+    normalization_factor = np.sum(F[:n+1], axis=1)
+    F[:n+1] /= normalization_factor[:, None]
+    F[n+1, 0] = F[n, 0] * rainx2 + F[n, 1] * (1-rainx2)
+    F[n+1, 1] = F[n, 1] * rainx2 + F[n, 0] * (1-rainx2)
+    F[n+2, 0] = F[n+1, 0] * rainx2 + F[n+1, 1] * (1-rainx2)
+    F[n+2, 1] = F[n+1, 1] * rainx2 + F[n+1, 0] * (1-rainx2)
     normalization_factor = np.sum(F, axis=1)
     F /= normalization_factor[:, None]
 
     return F
+
 observations = [int(x) for x in input().split()]
 result = forward_algorithm(observations)
 
-for t in range(len(observations)+1):
+for t in range(len(observations)+3):
     if t == 0:
-        print(f"Timestep {t}: 0.5")
+        print(f"Timestep {t}: 0.500")
     else:
         print(f"Timestep {t}: {result[t, 0]:.3f}")
